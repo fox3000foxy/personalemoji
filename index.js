551153ddff160e4c9b744e380ecaf3ccf3770b85
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2021-06-12 09:45:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', async ({ query }, response) => {
 					client_secret: clientSecret,
 					code,
 					grant_type: 'authorization_code',
-					redirect_uri: `https://personalemoji.fox3000.repl.co`,
+					redirect_uri: `https://personalemoji.herokuapp.com`,
 					scope: 'identify',
 				}),
 				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
